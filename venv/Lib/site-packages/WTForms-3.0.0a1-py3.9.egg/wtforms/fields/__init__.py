29460f0949b816f822f6a2ae2ebd5b1d5a8cd4ac
myusername <myemail>
2021-01-06 13:35:46 +0100

    adding virtual env

---
+++ venv/Lib/site-packages/WTForms-3.0.0a1-py3.9.egg/wtforms/fields/__init__.py
@@ -0,0 +1,7 @@
+from wtforms.fields.core import *
+from wtforms.fields.core import Field
+from wtforms.fields.core import Flags
+from wtforms.fields.core import Label
+from wtforms.fields.core import SelectFieldBase
+from wtforms.fields.simple import *
+from wtforms.utils import unset_value as _unset_value
